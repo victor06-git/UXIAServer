e3b6be8204d5319776a0c3d9539c416ffff52d13
Alvarjur <aalvarojur@gmail.com>
2026-02-02 17:08:28 +0100

    .

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 80;
 const _dirname = path.resolve();
 const bcrypt = require('bcrypt');
 
